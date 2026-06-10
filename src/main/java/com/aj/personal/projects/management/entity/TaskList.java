c93737d74ef2422e43c0db2c4504fc78f394f11d
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-10 09:21:56 +0100

    feat: completed tasklist  entity and table initialization

---
+++ src/main/java/com/aj/personal/projects/management/entity/TaskList.java
@@ -0,0 +1,60 @@
+package com.aj.personal.projects.management.entity;
+
+import java.math.BigDecimal;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasklists")
+public class TaskList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "tasklist")
+    private List<TaskListItem> tasklistitem;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    @Column(nullable = false)
+    private boolean completed;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public TaskList(User user, String name) {
+        this.user = user;
+        this.name = name;
+    }
+}
