b2cf507af8e75eea738cba70edc473894ac9ca01
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-10 09:23:43 +0100

    feat: completed user entity and table initialization

---
+++ src/main/java/com/aj/personal/projects/management/entity/User.java
@@ -0,0 +1,60 @@
+package com.aj.personal.projects.management.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<TaskList> lists;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public User(String email, String fullName,String userName, String password) {
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+        this.fullName = fullName;
+    }
+}
