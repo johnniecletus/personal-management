cd7f62fbbe754e69d5a7d61f8630daa023f4ffaa
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-10 09:23:18 +0100

    feat: completed tasklistitems entity and table initialization

---
+++ src/main/java/com/aj/personal/projects/management/entity/TaskListItem.java
@@ -0,0 +1,49 @@
+package com.aj.personal.projects.management.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasklistitems")
+public class TaskListItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "task_list_id", nullable = false)
+    private TaskList tasklist;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal amount = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    public TaskListItem(String name, BigDecimal amount, Boolean completed) {
+        this.name = name;
+        this.amount = amount == null
+                ? BigDecimal.ZERO
+                : amount;
+        this.completed = completed == null
+                ? false
+                : completed;
+    }
+}
