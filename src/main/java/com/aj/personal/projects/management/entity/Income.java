d0acefe4cfbdb6740980d4328bffbff178aee7a2
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-11 14:05:23 +0100

    fix: removal of income type from the income entity

--- src/main/java/com/aj/personal/projects/management/entity/Income.java
+++ src/main/java/com/aj/personal/projects/management/entity/Income.java
@@ -39,9 +39,6 @@ public class Income {
     private BigDecimal amount = BigDecimal.ZERO;
 
     @Column(nullable = false)
-    private String income_type;
-
-    @Column(nullable = false)
     private LocalDateTime month;
 
     @Column(nullable = false)
