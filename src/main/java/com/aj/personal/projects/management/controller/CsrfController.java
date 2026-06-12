6c4df718255c8c4ac761a070966f1774b2158191
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-12 16:49:23 +0100

    feat: completed csrf token get endpoint

--- src/main/java/com/aj/personal/projects/management/controller/CsrfController.java
+++ src/main/java/com/aj/personal/projects/management/controller/CsrfController.java
@@ -1,4 +1,15 @@
 package com.aj.personal.projects.management.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CsrfController {
+
+    @GetMapping("/api/v1/auth/csrf")
+    public CsrfToken csrf(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
 }
