186c8cb5e7d504dcfdba69407b8e40b2f507c668
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-12 16:50:08 +0100

    feat: completed csrf token get endpoint - response modification to follow our general response shape

--- src/main/java/com/aj/personal/projects/management/controller/CsrfController.java
+++ src/main/java/com/aj/personal/projects/management/controller/CsrfController.java
@@ -1,6 +1,9 @@
 package com.aj.personal.projects.management.controller;
 
+import com.aj.personal.projects.management.dto.ApiResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CsrfController {
 
     @GetMapping("/api/v1/auth/csrf")
-    public CsrfToken csrf(HttpServletRequest request) {
-        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    public ResponseEntity<ApiResponseDto<CsrfToken>> getCsrf(HttpServletRequest request) {
+        CsrfToken csrfTOken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+
+        ApiResponseDto<CsrfToken> response = ApiResponseDto.<CsrfToken>builder()
+                .success(true)
+                .data(csrfTOken)
+                .message("Csrf token fetched successfully")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
