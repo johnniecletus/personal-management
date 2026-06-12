738ed6661cff63491811b73601149b6667a31a94
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-12 12:44:44 +0100

    feat: customize the JwtAuthenticationEntryPoint file to return our type of error to maintain homogenity across the application

--- src/main/java/com/aj/personal/projects/management/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/aj/personal/projects/management/security/JwtAuthenticationEntryPoint.java
@@ -1,22 +1,54 @@
 package com.aj.personal.projects.management.security;
 
+import com.aj.personal.projects.management.dto.ErrorResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
-    ) throws IOException, ServletException {
-response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        ErrorResponseDto errorResponse =  ErrorResponseDto.builder()
+                .success(false)
+                .error("UNAUTHORIZED")
+                .message("You need to login to access this resource")
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        objectMapper.writeValue(
+                response.getWriter(),
+                errorResponse
+        );
+
+//        response.getWriter().write(
+//                """
+//                          {
+//                                      "success": false,
+//                                      "error": "UNAUTHORIZED",
+//                                      "message": "You need to login to access this resource"
+//                                    }
+//                        """
+//        );
     }
 }
