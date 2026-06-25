c3b4c2f6ebe708158db15f3c7c6265ff0e7e1042
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-06-25 08:58:55 +0100

    feat: separation of jacksonconfig and password encoder configuration files

--- src/main/java/com/aj/personal/projects/management/repository/SavingsClusterItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aj.personal.projects.management.repository;
-
-import com.aj.personal.projects.management.entity.SavingsClusterItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavingsClusterItemRepository extends JpaRepository<SavingsClusterItem, Long> {
-}
--- src/main/java/com/aj/personal/projects/management/repository/TaskListItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aj.personal.projects.management.repository;
-
-import com.aj.personal.projects.management.entity.TaskListItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskListItemRepository extends JpaRepository<TaskListItem, Long> {
-}
